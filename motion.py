3f39e9ace1557135e0508ce9713a520b8461b3a2
Rémi Boisseau <125018640+boisseau@users.noreply.github.com>
2023-02-15 18:01:26 +0100

    le robot bouge avec une boucle

--- motion.py
+++ motion.py
@@ -10,27 +10,15 @@ names = ["HeadYaw","HeadPitch"]
 fractionMaxSpeed= 0.2
 pause=0.7
 
-angles1 = [0.0,-pi/4]
-motionProxy.setAngles(names, angles1, fractionMaxSpeed)
-tts.say('haut')
-time.sleep(pause)
-angles2 = [0.0,pi/4]
-motionProxy.setAngles(names, angles2, fractionMaxSpeed)
-tts.say('bas')
-time.sleep(pause)
-angles3 = [pi/4,0.0]
-motionProxy.setAngles(names, angles3, fractionMaxSpeed)
-tts.say('gauche')
-time.sleep(pause)
-angles4 = [-pi/4,0.0]
-motionProxy.setAngles(names, angles4, fractionMaxSpeed)
-tts.say('droite')
-time.sleep(pause)
-angles5 = [0,0]
-motionProxy.setAngles(names, angles5, fractionMaxSpeed)
-tts.say('devant')
-#1=haut
-#2=bas
-#3=gauche
-#4=droite
-#5=normal
+angles_et_mots = [
+    ([0.0,-pi/4], 'haut'),
+    ([0.0,pi/4], 'bas'),
+    ([pi/4,0.0], 'gauche'),
+    ([-pi/4,0.0], 'droite'),
+    ([0.0,0.0], 'devant'),
+]
+
+for (angles, mot) in angles_et_mots:
+    motionProxy.setAngles(names, angles, fractionMaxSpeed)
+    tts.say(mot)
+    time.sleep(pause)
