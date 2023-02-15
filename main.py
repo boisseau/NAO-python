4d0f4c8f8dad970d2f92c7c7162dee6a6cb75c78
Rémi Boisseau <125018640+boisseau@users.noreply.github.com>
2023-02-15 12:24:32 +0100

    range au lieu d'énumérer les nombres

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ session = qi.Session()
 session.connect("192.168.0.158")
 tts = session.service('ALTextToSpeech')
 
-for patate in ('1', '2', '3'):
-    tts.say(patate)
+for patate in range(1,11):
+    tts.say(str(patate))
